c9571f56d7f316a9776226e2d6eef82ef4422b94
dgyc999 <134274850+yaliyang0102@users.noreply.github.com>
2025-11-06 02:10:26 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  webpack: (config) => {
+    // 避免可选依赖被解析（仅为降噪）
+    config.resolve.alias['pino-pretty'] = false;
+    return config;
+  }
 };
 module.exports = nextConfig;
