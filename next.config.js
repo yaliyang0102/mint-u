803e896d7be9349cfe0995c80da802fc3a71d80d
dgyc999 <134274850+yaliyang0102@users.noreply.github.com>
2025-11-07 17:41:21 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack: (config) => {
-    // 忽略仅 RN 场景需要的可选依赖
+    // 忽略仅 RN 场景需要的可选依赖，防止打包器去解析
     config.resolve.alias['@react-native-async-storage/async-storage'] = false;
     return config;
   },
